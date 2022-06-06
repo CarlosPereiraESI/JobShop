36d451a3da808243777a149adc18f703ffd5f3c7
CarlosPereiraESI <jocause@hotmail.com>
2022-06-06 08:29:25 +0100

    Changes

--- src/app.py
+++ src/app.py
@@ -3,8 +3,10 @@ from flask import Flask, abort, jsonify, request
 app = Flask(__name__)
 
 simulations = [
-    {'id': 1, 'nmachines': 3, 'njobs': 3, 'nops': 3}
-]
+    {'id': 1, 'nmachines': 3, 'njobs': 3, 'nops': 3}]
+
+jobs = []
+ops = []
 
 @app.route("/sims", methods=['GET'])
 def list_sims():
@@ -22,6 +24,14 @@ def create_sims():
     }
     if request.json['nmachines'] != request.json['njobs'] or request.json['nmachines'] != request.json['nops']:
         return({'result': "Machines, Jobs, Operations must be equal"}), 500
+    
+    numjobs = simulation['njobs']
+    for i in range(numjobs):
+        job = {
+            'id': i,
+            'operations': ops
+        }
+        jobs.append(job)
     simulations.append(simulation)
     return jsonify({'simulation': simulation}), 201
 
@@ -33,13 +43,57 @@ def delete_sim(id_sim):
     simulations.remove(simulation[0])
     return jsonify({'result': True})
 
-@app.route("/createtable/<int:id_sim>", methods=['POST'])
-def create_table(id_sim):
+# @app.route("/createtable/<int:id_sim>", methods=['POST'])
+# def create_table(id_sim):
+#     simulation = [simulation for simulation in simulations if simulation['id'] == id_sim]
+#     if len(simulation) == 0:
+#         return jsonify({'result': "Simulation not found"}), 404
+#     numjobs = simulation[0]['njobs']
+#     numops = simulation[0]['nops']
+
+#     for k in range(numops):
+#         op = {
+#             'id': k,
+#             'machine': request.json['machine'],
+#             'time': request.json['time']
+#         }
+#         ops.append(op)
+
+#     for n in range(numjobs):
+#         job = {
+#             'id': n,
+#             'operations': ops
+#         }
+#         jobs.append(job)
+#     return jsonify({'jobs': jobs})
+
+
+@app.route("/addoperation/<int:id_sim>/<int:id_job>", methods=['POST'])
+def add_op(id_sim, id_job):
     simulation = [simulation for simulation in simulations if simulation['id'] == id_sim]
     if len(simulation) == 0:
         return jsonify({'result': "Simulation not found"}), 404
+    numops = simulation[0]['nops']
+
+    job = [job for job in jobs if job['id'] == id_job]
+    if len(job) == 0:
+       return jsonify({'result': "Job not found"}), 404 
     
+    if not request.json or not 'id' or (request.json['id'] > numops) or not 'machine' or not 'time' in request.json:
+        abort(400)
+    operation = {
+        'id': request.json['id'],
+        'machine': request.json['machine'],
+        'time': request.json['time'],
+    }
+    ops.append(operation)
+    jobs.append(['operations'])
+    return jsonify({'operation': operation})
+
+
+@app.route("/table", methods=['GET'])
+def table():
+    return jsonify({'jobs': jobs})
 
 if __name__ == '__main__':
     app.run(debug=True)
-
